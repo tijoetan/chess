7a01a6271eaa110b862867b36c1f3e10f66c7bb4
Pranav Rao <praopro2017@gmail.com>
2023-12-27 13:27:13 -0800

    I hate OOP

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from Data import *
 from Board import Board, Player
 import pygame
-pygame.init()
 
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Chees")
-sprites = pygame.sprite.Group
-board = Board()
-white = Player(board, 'white')
-black = Player(board, 'black')
-print(board.tiles)
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption("Chees")
+        self.sprites = pygame.sprite.Group
+        self.board = Board()
+        self.white = Player(self.board, 'white')
+        self.black = Player(self.board, 'black')
+        print(self.board.tiles)
+        self.clock = pygame.time.Clock()
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            self.screen.fill('blue')
+            self.board.render()
+            self.white.get_click()
+            self.clock.tick(60)
+            pygame.display.update()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-    screen.fill('blue')
-    board.render()
-    white.get_click()
-    clock.tick(60)
-    pygame.display.update()
+if __name__ == '__main__':
+    game = Game()
+    game.run()
